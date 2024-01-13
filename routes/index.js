bb19b009f5e96327b9c90c056f358b3849de77d4
Anthuan <amorera@emerytelcom.com>
2024-01-13 15:49:50 -0700

    final test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 
 router.use("./users", require("./users"));
 
-// router.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 module.exports = router;
