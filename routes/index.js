970d9c4587cffc047f134d4e6774c09d1fd0bf6f
Anthuan <amorera@emerytelcom.com>
2024-01-13 15:31:21 -0700

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+router.use("./users", require("./users"));
+
+// router.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 module.exports = router;
