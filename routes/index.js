6634d47c45eac2b2d48159772420dffd390dbdf8
Anthuan <amorera@emerytelcom.com>
2024-01-13 16:02:10 -0700

    Last commit for localhost

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.use("./users", require("./users"));
+router.use("/users", require("./users"));
 
 router.get("/", (req, res) => {
   res.send("Hello World");
